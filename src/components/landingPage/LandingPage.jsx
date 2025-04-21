54379d8d23a1b3d4458709cbdcf7c74a09fe5724
Tyler314 <tjroberts314@gmail.com>
2025-04-20 19:26:16 -0500

    implement infinite horizontal scroll

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -4,6 +4,13 @@ import Header from "../header/Header"
 import Footer from "../footer/Footer"
 import "./LandingPage.css"
 
+const images = [
+    "/dummy-pictures/pic1.jpg",
+    "/dummy-pictures/pic2.jpg",
+    "/dummy-pictures/pic3.jpg",
+    "/dummy-pictures/pic4.jpg"
+];
+
 
 function Title() {
     return (
@@ -29,11 +36,43 @@ function Title() {
     )
 }
 
+function InfiniteCarousel() {
+    const doubledImages = [...images, ...images];
+
+    return (
+        <div className="carousel-container">
+            <motion.div
+                className="carousel-track"
+                animate={{ x: ["0%", "-50%"] }}
+                transition={{
+                    duration: 50,
+                    ease: "linear",
+                    repeat: Infinity,
+                }}
+            >
+                {doubledImages.map((src, idx) => (
+                    <img
+                        src={src}
+                        alt={`carousel-${idx}`}
+                        className="carousel-image"
+                    />
+                ))}
+            </motion.div>
+        </div>
+    );
+}
+
 export default function LandingPage() {   
     return (
         <div className="landing-page-wrapper">
             <Header/>
             <Title/>
+            <img
+                className='read-more-artist-image'
+                src='/dummy-pictures/banner.jpg'
+                alt=''
+            />
+            <InfiniteCarousel/>
             <Footer/>
         </div>
     )
