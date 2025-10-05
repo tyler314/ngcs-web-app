d6741323b068637c54fecba44110da4ce5691f0c
Tyler314 <tjroberts314@gmail.com>
2025-10-04 22:11:28 -0500

    update IMAGE_API lambda function

--- src/common/constants.jsx
+++ src/common/constants.jsx
@@ -1,5 +1,5 @@
 const IMAGE_API =
-  "https://l4ntih2s43iaybgsadwp5b2xka0yxyup.lambda-url.us-east-2.on.aws";
+  "https://byjn6jzhbvipxyu6qkyh2gbnn40ltgvn.lambda-url.us-east-2.on.aws/";
 
 const BANNER_IMAGE_URI =
   "https://ngcs-website-images.s3.us-east-2.amazonaws.com/landing-page-banner.jpg";
@@ -13,10 +13,10 @@ const INSTRUCTORS_API = "https://kxanxtcodnphw6o2txqqphuede0ipyzm.lambda-url.us-
 const S3_INSTRUCTORS_BUCKET_URL = "https://ngcs-instructors.s3.us-east-2.amazonaws.com/";
 const DEFAULT_INSTRUCTOR_IMAGE = "default-instructor.jpg";
 
-export { 
-  IMAGE_API, 
-  BANNER_IMAGE_URI, 
-  GYM_SCHEDULE, 
+export {
+  IMAGE_API,
+  BANNER_IMAGE_URI,
+  GYM_SCHEDULE,
   INSTRUCTORS_API,
   S3_INSTRUCTORS_BUCKET_URL,
   DEFAULT_INSTRUCTOR_IMAGE
